27b3da1d1f58a3fe593adc77cca103514b275961
Averill Cate, Jr <acatejr@gmail.com>
2018-03-14 08:12:00 -0700

    Client work.

---
+++ client/src/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <h2>EAPI</h2>
+  </div>
+</template>
+
+<script>
+module.exports = {
+   data: function () {
+      return {}
+   }
+}
+</script>
+
+<style scoped>
+</style>
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import Home from './Home.vue'
+
+Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   el: '#app',
+  components: {
+    home: Home
+  },
   render() {
-    return <h1>Hello Vue.</h1>
+    return <div class='container'>
+      <home />
+      </div>
   }
 })
