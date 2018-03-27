7a32dfeac0280f5cbd905121fb495cf553516631
Averill Cate, Jr <acatejr@gmail.com>
2018-03-27 12:48:15 -0700

    Working on loading precip data.

--- manage.py
+++ manage.py
@@ -1,16 +1,94 @@
+#!/usr/bin/env python
 import math, os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import app, db
 from data.convert_coords import utm_to_latlon
-app.config.from_object(os.environ['APP_SETTINGS'])
 
+app.config.from_object(os.environ['APP_SETTINGS'])
 manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 # Import models here
-from models import Raingage
+from models import Raingage, PrecipEvent
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+station_name_map = [
+    {"id": "white", "name": "Whitehouse"},
+    {"id": "watre", "name": "Water Retention"},
+    {"id": "sw", "name": "Southwest"},
+    {"id": "scs5n", "name": "SCS-5N"},
+    # {"id": "scs#1", "name": "SCS" },
+    {"id": "scs#1", "name": "SCS #1"},
+    {"id": "ruela", "name": "Ruelas"},
+    {"id": "roden", "name": "Rodent"},
+    {"id": "robin", "name": "Robinson"},
+    {"id": "road", "name": "Road"},
+    {"id": "past3", "name": "Pasture 3"},
+    {"id": "parke", "name": "Parker"},
+    {"id": "pa11a", "name": "Pasture 11A"},
+    {"id": "nw", "name": "Northwest"},
+    {"id": "nrnch", "name": "North Ranch"},
+    {"id": "ne", "name": "Northeast"},
+    {"id": "muhle", "name": "Muhlenbergia"},
+    {"id": "mcgib", "name": "McGibbon"},
+    {"id": "limst", "name": "Limestone"},
+    {"id": "johns", "name": "Johnson Ranch"},
+    {"id": "ibp", "name": "IBP"},
+    {"id": "hughe", "name": "Hughes"},
+    {"id": "huerf", "name": "Huerfano"},
+    {"id": "grari", "name": "Gravelly Ridge"},
+    {"id": "fores", "name": "Forest"},
+    {"id": "flori", "name": "Florida"},
+    {"id": "fagan", "name": "Fagan"},
+    {"id": "eriop", "name": "Eriopoda"},
+    {"id": "desst", "name": "Desert Station"},
+    {"id": "desri", "name": "Desert Rim"},
+    {"id": "desgr", "name": "Desert Grassland"},
+    {"id": "choll", "name": "Cholla"},
+    {"id": "box", "name": "Box"},
+    {"id": "amado", "name": "Amado"},
+    {"id": "airst", "name": "Airstrip"},
+    {"id": "164", "name": "Enclosure 164"},
+    {"id": "45", "name": "Enclosure 45"},
+    {"id": "41", "name": "Enclosure 41"},
+    {"id": "205", "name": "Enclosure 205"},
+    {"id": "18", "name": "Enclosure 18"},
+    {"id": "131", "name": "Encl. No. 131"},
+]
+
+def find_station_name(name):
+    station_name = None
+
+    for s in station_name_map:
+        if s['id'] == name:
+            station_name = s['name']
+
+    return station_name
+
+@manager.command
+def load_precip_events():
+    Raingage.query.delete()
+    print("Loading precip event data.")
+    l = 0
+    with open('./data/precip.csv', 'r') as f:
+        for line in f.readlines():
+            if l > 0:
+                values = line.strip().split('|')
+                station = values[0].lower()
+                year = values[1]
+                station_name = None
+                station_id_list = list(i['id'] for i in station_name_map)
+                if station in station in station_id_list:
+                    station_name = find_station_name(station)
+                else:
+                    station_name = station
+                print(station_name)
+
+            l += 1
+
+    print("Done!")
 
 @manager.command
 def load_raingages():
