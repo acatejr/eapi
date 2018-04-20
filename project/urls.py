cc33a2ec23c5320e991b97a2b90968299fe3b6bb
Averill Cate, Jr <acatejr@gmail.com>
2018-04-20 15:03:53 -0700

    Enabled REST and REST Swagger.

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='EAPI API')
+schema_view = get_swagger_view(title='EAPI')
 
 urlpatterns = [
     path('', include('apphome.urls', namespace='home')),
