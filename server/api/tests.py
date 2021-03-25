cbd10d77f68973e4ca0c2930196cb94b66ee82ea
Averill Cate, Jr <acate@arizona.edu>
2021-03-24 17:30:37 -0700

    Got graphql/graphene running

--- server/api/tests.py
+++ server/api/tests.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
+from graphene.test import Client
+from api.schema import schema
 import pytest
 
+def test_api_index():
+    client = Client(schema)
+    query = '''{status}'''
+    response = client.execute(query)
 
-@pytest.mark.django_db
-def test_api_index(client):
-    """Test the index or root index of the api"""
-    url = reverse("graphql")
-    response = client.get(url)
-    assert response.status_code == 200
+    assert response == {'data': {'status': 'status'}}
+
+# @pytest.mark.django_db
+# def test_api_index(client):
+#     """Test the index or root index of the api"""
+#     url = reverse("graphql")
+#     response = client.get(url)
+#     assert response.status_code == 200
