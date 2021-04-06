9bc9169c82e73dd10821ed4db89bf8814f0403db
Averill Cate, Jr <acate@arizona.edu>
2021-04-06 15:25:33 -0700

    Added wgew raingage model and data loading

--- server/apps/wgew/admin.py
+++ server/apps/wgew/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Raingage
 
-# Register your models here.
+@admin.register(Raingage)
+class RaingageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'watershed_id', 'gage_id', 'elevation', 'created', 'updated']
+    list_filter = ['watershed_id', 'gage_id',]
+
+# class PrecipEventInline(admin.TabularInline):
+#     model = PrecipEvent
+
+
+# @admin.register(Raingage)
+# class RaingageAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'watershed_id', 'gage_id', 'elevation', 'created', 'updated']
+#     list_filter = ['watershed_id', 'gage_id',]
+#     inlines = [PrecipEventInline]
+
+
+# @admin.register(PrecipEvent)
+# class PrecipEventAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'raingage', 'event_date', 'event_time', 'depth']
+#     list_per_page = 15
