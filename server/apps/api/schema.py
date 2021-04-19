3922104c579a7a7dfb6a9ae474ff0e38700e441b
Averill Cate, Jr <acate@arizona.edu>
2021-04-19 07:47:51 -0700

    Setup for fastapi branch

--- server/apps/api/schema.py
+++
@@ -1,9 +0,0 @@
-import graphene
-
-class Query(graphene.ObjectType):
-    status = graphene.String()
-
-    def resolve_status(root, info):
-        return "status"
-
-schema = graphene.Schema(query=Query)
