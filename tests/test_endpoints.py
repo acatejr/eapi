c1469f67604aa69e9e70f93af1d290ecc5c8d2a3
Averill Cate, Jr <acate@arizona.edu>
2021-04-20 16:56:16 -0700

    Implemented testing for main api endpoint

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,5 +1,11 @@
 import pytest
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
 
 @pytest.mark.skip("Skipping api endpoint tests.")
 def test_api_root():
-   pytest.fail("Implement me!")
+    response = client.get("/")
+    assert response.status_code == 200
