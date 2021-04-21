a649096e37ea86a515e0167e9cfce39770037743
Averill Cate, Jr <acate@arizona.edu>
2021-04-21 14:57:38 -0700

    Base level testing setup

--- tests/test_wgew_models.py
+++ tests/test_wgew_models.py
@@ -3,6 +3,16 @@ import pytest
 def test_create_raingage():
     pytest.fail("Implement me!")
 
-def test_create_precip_event():
+def test_read_raingage():
     pytest.fail("Implement me!")
 
+def test_udpdate_raingage():
+    pytest.fail("Implement me!")
+
+def test_delete_raingage():
+    pytest.fail("Implement me!")
+
+"""
+def test_create_precip_event():
+    pytest.fail("Implement me!")
+"""
