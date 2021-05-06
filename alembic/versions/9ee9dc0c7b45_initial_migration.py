b8c0511eccd4e1f150f04bc299e49a329db02fc8
Averill Cate, Jr <acatejr@gmail.com>
2021-05-06 19:04:36 +0000

    Alembic reset

---
+++ alembic/versions/9ee9dc0c7b45_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: 9ee9dc0c7b45
+Revises: 1019c04a7a22
+Create Date: 2021-05-06 18:30:10.961840
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9ee9dc0c7b45'
+down_revision = '1019c04a7a22'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
