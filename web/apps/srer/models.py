807ae4ef0dc8c81eb35ae4837ac7b5eaa6b32486
Averill Cate, Jr <acatejr@gmail.com>
2021-07-12 21:19:00 +0000

    Added srer app

---
+++ web/apps/srer/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Raingage(models.Model):
+    pass
+
--- web/web/settings.py
+++ web/web/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'apps.wgew',
+    'apps.srer',
 ]
 
 MIDDLEWARE = [
