76ff411156378e7d97d7898edbf0ef63ebf90798
Averill Cate, Jr <acatejr@gmail.com>
2021-07-12 21:07:05 +0000

    Finished model tests for WGEW

--- web/apps/wgew/test_wgew_models.py
+++ web/apps/wgew/test_wgew_models.py
@@ -10,18 +10,58 @@ class TestModels:
             gage_id=1,
             watershed_id=1
         )
-
-    def test_precipevent_create(self):
-        """
-        Raingage.objects.create(
+   
+    def test_raingage_read(self):
+        instance = Raingage.objects.create(
             gage_id=1,
             watershed_id=1
         )
-        """
+        rg = Raingage.objects.get(id=instance.id)
+        assert rg is not None
 
-        """
-        rg = Raingage(gage_id=1, watershed_id=1)
+    def test_raingage_update(self):
+        instance = Raingage.objects.create(
+            gage_id=1,
+            watershed_id=1
+        )
+
+        rg = Raingage.objects.get(id=instance.id)
+        rg.watershed_id = 2
         rg.save()
-        """
-        
-        PrecipEvent.objects.create()
+
+        assert rg.watershed_id == 2
+ 
+    def test_raingage_delete(self):
+        rg = Raingage.objects.create(
+            gage_id=1,
+            watershed_id=1
+        )
+
+        assert rg is not None
+        rg.delete()
+
+        with pytest.raises(Raingage.DoesNotExist):
+            Raingage.objects.get(id=1)
+  
+    def test_precipevent_create(self):
+        PrecipEvent.objects.create(duration=2.3)
+
+    def test_precipevent_read(self):
+        instance = PrecipEvent.objects.create(duration=1)
+        pe = PrecipEvent.objects.get(id=instance.id)
+        assert pe is not None
+
+    def test_precipevent_update(self):
+        instance = PrecipEvent.objects.create(duration=1)
+        instance.duration = 2
+        instance.save()
+
+        pe = PrecipEvent.objects.get(id=instance.id)
+        assert pe.duration == instance.duration
+
+    def test_precipevent_delete(self):
+        pe = PrecipEvent.objects.create(id=1)
+        pe.delete()
+        with pytest.raises(PrecipEvent.DoesNotExist):
+            PrecipEvent.objects.get(id=1)
+
