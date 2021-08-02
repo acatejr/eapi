22e940444555a20ddefa5d44a0fbecfdc2f82d75
Averill Cate, Jr <acatejr@gmail.com>
2021-08-02 23:44:59 +0000

    Addes SRER raingages to graphql api

--- web/schema.py
+++ web/schema.py
@@ -2,7 +2,13 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from apps.wgew.models import Raingage as WGEWRaingage
+from apps.srer.models import Raingage as SRERRaingage
 
+class SRERRaingageType(DjangoObjectType):
+
+    class Meta:
+        description = 'SRER Raingage'
+        model = SRERRaingage
 
 class WGEWRaingageType(DjangoObjectType):
 
@@ -14,9 +20,13 @@ class WGEWRaingageType(DjangoObjectType):
 class Query(graphene.ObjectType):
     """Returns all WGEW raingages"""
     all_wgew_raingages = graphene.List(WGEWRaingageType, description="Walnut Gulch Exerimental Watershed Raingage")
+    all_srer_raingages = graphene.List(SRERRaingageType, description="Santa Rita Experimental Range Raingage")
 
     def resolve_all_wgew_raingages(root, info):
         return WGEWRaingage.objects.all()
+
+    def resolve_all_srer_raingages(root, info):
+        return SRERRaingage.objects.all()
 
 
 schema = graphene.Schema(query=Query)
