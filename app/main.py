2b71c04bbcacf3952dcc57902da2e278725ce59b
Averill Cate, Jr <acatejr@gmail.com>
2021-12-11 10:51:49 -0700

    WGEW rest-swagger routes started

--- app/main.py
+++ app/main.py
@@ -5,23 +5,31 @@ app = FastAPI()
 
 @app.get("/healthcheck")
 async def health_check():
-    msg = "ok" 
+    msg = "ok"
     return {"msg": msg}
 
-@app.get("/api/v1/gages/")
-async def gages():
+@app.get("/api/v1/wgewgages/")
+async def wgewgages():
     """Get all raingages."""
 
-    payload = [
+    data = [
         {"wsid": 1, "gageid": 1},
         {"wsid": 2, "gageid": 2}
 
     ]
-    return {"payload": payload}
+    return {"data": data}
 
-@app.get("/api/v1/gages/{wsid}/{gageid}")
-async def gages(wsid: int, gageid: int):
+@app.get("/api/v1/wgewgages/{wsid}/{gageid}")
+async def wgewgages(wsid: int, gageid: int):
     """Get a raingage by watershed id and raingage id.
     """
-    payload = {"wsid": 1, "gageid": 1}
-    return {"payload": payload}
+    data = {"wsid": 1, "gageid": 1}
+    return {"payload": data}
+
+@app.get("/api/v1/wgewprecip/{wsid}/{gageid}")
+async def wgewprecip(wsid: int, gageid: int):
+    """Retrieves the specified watershed and raingage precip events.
+    """
+
+    data = []
+    return {"data": data}
--- tests/test_api.py
+++ tests/test_api.py
@@ -11,12 +11,19 @@ def test_health_check():
     assert resp.status_code == 200
     assert resp.json() == {"msg": "ok"}
 
-def test_api_v1_gages():
+def test_api_v1_wgew_gages():
     """The api must allow for retrieval of 1 one or all raingages
     for a specified watershed.
     """
-    resp = client.get("/api/v1/gages/")
+    resp = client.get("/api/v1/wgewgages/")
     assert resp.status_code == 200
 
-    resp = client.get("/api/v1/gages/1/1")
+    resp = client.get("/api/v1/wgewgages/1/1")
+    assert resp.status_code == 200
+
+def test_api_v1_wgew_precip():
+    """The api must allow for retrieval of precip events for a
+    specified watershed id and raingage id"""
+
+    resp = client.get("/api/v1/wgewprecip/1/1")
     assert resp.status_code == 200
