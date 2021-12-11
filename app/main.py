e212a4289a9f4cead8004768ea74b56bf1af53e6
Averill Cate, Jr <acatejr@gmail.com>
2021-12-10 17:21:48 -0700

    Have some api v1 routes in place

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 @app.get("/healthcheck")
 async def health_check():
-    msg = None
-    return {
-        "msg": msg
-    }
+    msg = "ok" 
+    return {"msg": msg}
 
+@app.get("/api/v1/gages/")
+async def gages():
+    """Get all raingages."""
+
+    payload = [
+        {"wsid": 1, "gageid": 1},
+        {"wsid": 2, "gageid": 2}
+
+    ]
+    return {"payload": payload}
+
+@app.get("/api/v1/gages/{wsid}/{gageid}")
+async def gages(wsid: int, gageid: int):
+    """Get a raingage by watershed id and raingage id.
+    """
+    payload = {"wsid": 1, "gageid": 1}
+    return {"payload": payload}
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,22 @@
 import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
 
 def test_health_check():
-    pytest.fail("Implement me!")
+    """Thje api must have a healthcheck route.
+    """
+    resp = client.get("/healthcheck")
+    assert resp.status_code == 200
+    assert resp.json() == {"msg": "ok"}
+
+def test_api_v1_gages():
+    """The api must allow for retrieval of 1 one or all raingages
+    for a specified watershed.
+    """
+    resp = client.get("/api/v1/gages/")
+    assert resp.status_code == 200
+
+    resp = client.get("/api/v1/gages/1/1")
+    assert resp.status_code == 200
