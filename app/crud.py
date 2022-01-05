547ccd80a4e9ea551c998b46380b445c73855b5b
Averill Cate, Jr <acatejr@gmail.com>
2022-01-05 16:38:59 -0700

    WGEW watershed adn gage precip events route works

--- app/crud.py
+++ app/crud.py
@@ -6,15 +6,19 @@ def get_wgew_raingages(db: Session):
     return db.query(models.WGEWRaingage).all()
 
 def get_wgew_watershed_raingage(db: Session, wsid: int, gageid: int):
-    
+
     return db.query(models.WGEWRaingage).filter(
             models.WGEWRaingage.watershed_id == wsid
         ).filter(
             models.WGEWRaingage.gage_id == gageid
         ).first()
 
-def get_wgew_watershed_gage_precip(db: Session, wsid, gageid):
+def get_wgew_watershed_gage_precip(db: Session, wsid: int, gageid: int):
     """Retrieve all precip events for given watershed and raingage.
     """
 
-    return None
+    return db.query(models.WGEWPrecipEvent).filter(
+            models.WGEWPrecipEvent.gage_id == gageid
+        ).filter(
+            models.WGEWPrecipEvent.watershed_id == wsid
+        ).all()
